a73f0ab153353c87d4a807d73f2f0870b8cfe8ce
AndruQuiroga <44418373+AndruQuiroga@users.noreply.github.com>
2025-09-06 00:40:05 -0400

    Add test configuration and CI

---
+++ noxfile.py
@@ -0,0 +1,27 @@
+import nox
+
+PYTHON_VERSIONS = ["3.10", "3.11"]
+LOCATIONS = ["services", "sidetrack", "tests", "noxfile.py"]
+
+nox.options.sessions = ["lint", "unit", "integration"]
+nox.options.reuse_existing_virtualenvs = True
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def unit(session: nox.Session) -> None:
+    session.install("-r", "requirements-dev.txt")
+    session.install("-e", ".")
+    session.run("pytest", "-m", "unit")
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def integration(session: nox.Session) -> None:
+    session.install("-r", "requirements-dev.txt")
+    session.install("-e", ".")
+    session.run("pytest", "-m", "integration")
+
+
+@nox.session(python=PYTHON_VERSIONS)
+def lint(session: nox.Session) -> None:
+    session.install("pre-commit")
+    session.run("pre-commit", "run", "--all-files")
